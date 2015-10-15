68e68f08c34058f6e13b245376cf95708fc428df
vansanjp <vank58.bk@gmail.com>
2015-10-15 23:15:37 +0700

    SourceCode Server

--- SourceCode/buttonflag/ButtonFlag.java
+++
@@ -1,8 +0,0 @@
-package buttonflag;
-
-public class ButtonFlag{
-	public int buttonQueryFlag = 1;
-	public int buttonInsertFlag = 1;
-	public int buttonDeleteFlag = 1;
-	public int buttonUpdateFlag = 1;
-}
