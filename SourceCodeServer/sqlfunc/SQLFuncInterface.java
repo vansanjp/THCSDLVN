2caf5297a699faec4e0c1a0493c50081d9ddaef6
vansanjp <vank58.bk@gmail.com>
2015-10-21 18:11:07 +0700

    giao dien sqlfunc

---
+++ SourceCodeServer/sqlfunc/SQLFuncInterface.java
@@ -0,0 +1,11 @@
+package sqlfunc;
+
+public interface SQLFuncInterface {
+	public String url = "jdbc:mysql://localhost:3306/BT_LON";
+	public String username = "root";
+	public String password = "mured";
+	public final String standardCharacter1 = "abcdefghijklmnopqrstuvxywz";
+	public final String standardCharacter2 = "ABCDEFGHIJKLMNOPQRSTUVXYWZ";
+	public final String standardCharacter3 = "0123456789_";
+	public final String standardCharacter4 = ",";
+}
